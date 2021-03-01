5055e3cfd7ef9018bf796056ea7c005c1f256b6c
Edyta Kaleta <edyta.anna.kaleta@gmail.com>
2021-03-01 17:37:10 +0100

    modification of the code layout in forms.py according to PEP 8

--- carbon_footprint_calculator/forms.py
+++ carbon_footprint_calculator/forms.py
@@ -9,27 +9,63 @@ def AmountValidator(value):
 
 
 class HouseForm(forms.Form):
-    household = forms.ChoiceField(label="Liczba osób w gospodarstwie domowym", choices=NUMBER_OF_PEOPLE_IN_HOUSEHOLD)
-    type_of_heating = forms.ChoiceField(label="Forma ogrzewania", choices=House.TYPE_OF_HEATING)
-    amount = forms.IntegerField(label="Wartość zurzyta", validators=[AmountValidator])
+    household = forms.ChoiceField(
+        label="Liczba osób w gospodarstwie domowym",
+        choices=NUMBER_OF_PEOPLE_IN_HOUSEHOLD
+    )
+    type_of_heating = forms.ChoiceField(
+        label="Forma ogrzewania",
+        choices=House.TYPE_OF_HEATING)
+    amount = forms.IntegerField(
+        label="Wartość zurzyta",
+        validators=[AmountValidator]
+    )
 
 
 class TravelForm(forms.Form):
-    mode_of_transport = forms.ChoiceField(label="Środek transportu", choices=Travel.MEANS_OF_TRANSPORT)
-    length_of_distance = forms.IntegerField(label="Pokonany dystans w km", validators=[AmountValidator])
+    mode_of_transport = forms.ChoiceField(
+        label="Środek transportu",
+        choices=Travel.MEANS_OF_TRANSPORT
+    )
+    length_of_distance = forms.IntegerField(
+        label="Pokonany dystans w km",
+        validators=[AmountValidator]
+    )
     countries = Country.objects.all().order_by("name")
-    countries_choices = [(country.equivalent, country.name) for country in countries]
-    country = forms.ChoiceField(label="Kraj zakwaterowania", choices=countries_choices)
-    number_of_night = forms.IntegerField(label="Liczba nocy", validators=[AmountValidator])
+    countries_choices = [
+        (country.equivalent, country.name) for country in countries
+    ]
+    country = forms.ChoiceField(
+        label="Kraj zakwaterowania",
+        choices=countries_choices
+    )
+    number_of_night = forms.IntegerField(
+        label="Liczba nocy",
+        validators=[AmountValidator]
+    )
 
 
 class TransportForm(forms.Form):
-    form_of_moving = forms.ChoiceField(choices=DailyTransport.DAILY_MEANS_OF_TRANSPORT, label="Środek transportu")
-    distance_in_km = forms.IntegerField(label="Pokonany dystans w km", validators=[AmountValidator])
+    form_of_moving = forms.ChoiceField(
+        choices=DailyTransport.DAILY_MEANS_OF_TRANSPORT,
+        label="Środek transportu"
+    )
+    distance_in_km = forms.IntegerField(
+        label="Pokonany dystans w km",
+        validators=[AmountValidator]
+    )
 
 
 class FoodForm(forms.Form):
     products = Food.objects.all().order_by("name_food")
-    products_choices = [(product.equivalent, product.name_food) for product in products]
-    product = forms.ChoiceField(label="Nazwa produktu", choices=products_choices)
-    consumption = forms.IntegerField(label="Spożycie w kaloriach", validators=[AmountValidator])
+    products_choices = [
+        (product.equivalent, product.name_food) for product in products
+    ]
+    product = forms.ChoiceField(
+        label="Nazwa produktu",
+        choices=products_choices
+    )
+    consumption = forms.IntegerField(
+        label="Spożycie w kaloriach",
+        validators=[AmountValidator]
+    )
