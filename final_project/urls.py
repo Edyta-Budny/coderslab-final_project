6ac5ce5ad4f66ff8e6b73be910d11a071f18b1af
Edyta Kaleta <edyta.anna.kaleta@gmail.com>
2021-03-01 12:48:58 +0100

    add new urlpattern

--- final_project/urls.py
+++ final_project/urls.py
@@ -4,10 +4,11 @@ from django.urls import path
 from carbon_footprint_calculator.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', MainView.as_view(), name="main"),
     path('main/', MainView.as_view(), name="main"),
     path('heating_home_or_flat/', HouseView.as_view(), name="home"),
     path('travel/', TravelView.as_view(), name="travel"),
     path('transport/', TransportView.as_view(),  name="transport"),
     path('diet/', DietView.as_view(), name="diet"),
+    path('admin/', admin.site.urls),
 ]
