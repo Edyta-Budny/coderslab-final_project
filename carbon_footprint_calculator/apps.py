280985b667e149b5e3116ce6eac28827711c90b6
Edyta Kaleta <edyta.anna.kaleta@gmail.com>
2021-03-22 23:29:27 +0100

    configure default_auto_field on a per-app basis

--- carbon_footprint_calculator/apps.py
+++ carbon_footprint_calculator/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class CarbonFootprintCalculatorConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'carbon_footprint_calculator'
